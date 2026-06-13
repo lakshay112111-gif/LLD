18cbe07c59ec27a65b1ba469e741178ab5e34748
lakshay112111-gif <lakshay112111@gmail.com>
2026-06-13 23:44:15 +0530

    Update Singleton.cs

--- Singleton.cs
+++ Singleton.cs
@@ -6,11 +6,33 @@
 // We use static, member and method to achieve this since, we can't create a object of the class since the constructor will be private, so we need to use static keyword, for that.
 // And why we are using, here Sealed class because we don't want any other class to inherit it: is it necessary :NO (For safety purpose only).
 
+
+// THREADSAFE: BUT STILL EDGECASE, COST HEAVY.
+
+//    private static readonly object lockObj = new object();
+//  lock (lockObj)
+//         {
+//             if (instance == null)
+//             {
+//                 instance = new ThreadSafeSingleton();
+//             }
+//             return instance;
+//         }
+
 using System;
 
 public sealed class Singleton {
     
-    public static Singleton _instance;
+    // public static Singleton _instance;
+    
+    // Now, this line makes it eager initialization.
+    
+    // Can use, Lazy instantiation, for maximum output since the object will always be created due to current, eager initialization. 
+    
+    // private static readonly Lazy<Singleton> _instance =
+    // new Lazy<Singleton>(() => new Singleton());
+    
+      private static readonly Singleton _instance = new Singleton();
     
     private Singleton(){}
     
@@ -21,10 +43,12 @@ public sealed class Singleton {
     
     public static Singleton getInstance(){
         
-        if(_instance == null){
+        // Eager initilization.
+        
+        // if(_instance == null){
             
-            _instance = new Singleton();
-        }
+        //     _instance = new Singleton();
+        // }
         
         return _instance;
     }
