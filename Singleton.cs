77a9c559aef38474b38e0f0d20c94a45510b37ae
lakshay112111-gif <lakshay112111@gmail.com>
2026-06-13 23:19:08 +0530

    Create Singleton.cs

---
+++ Singleton.cs
@@ -0,0 +1,44 @@
+
+
+
+// Singleton Design Pattren implementation:
+// 0nly one instance of the class should be created, thats the goal.
+// We use static, member and method to achieve this since, we can't create a object of the class since the constructor will be private, so we need to use static keyword, for that.
+// And why we are using, here Sealed class because we don't want any other class to inherit it: is it necessary :NO (For safety purpose only).
+
+using System;
+
+public sealed class Singleton {
+    
+    public static Singleton _instance;
+    
+    private Singleton(){}
+    
+    public void Print(){
+        
+        Console.WriteLine("The object is created!!");
+    }
+    
+    public static Singleton getInstance(){
+        
+        if(_instance == null){
+            
+            _instance = new Singleton();
+        }
+        
+        return _instance;
+    }
+}
+
+public class Program {
+    
+    public static void Main(){
+        
+        Singleton s1 = Singleton.getInstance();
+        Singleton s2 = Singleton.getInstance();
+        
+        s1.Print();
+        
+        Console.WriteLine(object.ReferenceEquals(s1, s2));
+    }
+}
